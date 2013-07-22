93e9abf088e761707abcc42bf68f9a3889f2737d
Otto Velander <o.velander@gmail.com>
2013-07-22 14:21:50 +0200

    changed more stuff

--- app/assets/javascripts/misc/profileplatescheckbox.js
+++ app/assets/javascripts/misc/profileplatescheckbox.js
@@ -1,1 +1,44 @@
+checked=false;
+secondarychecked=false;
+
+function checkAllPlates() {
+
+	var cp = document.getElementsByClassName('plates_check_all');
+	var aa = document.getElementsByClassName('plates_check');
+	checked=cp[0].checked;
+	 if (checked == true)
+          {
+           checked = true
+          }
+        else
+          {
+          checked = false
+          }
+	for (var i=0; i < aa.length; i++) 
+	{
+	 
+	 aa[i].checked = checked;
+	}
+}
+
+
+
+
+function checkAllSecondaryPlates() {
+	var sp= document.getElementsByClassName('secondary_plates_check');
+    var scp= document.getElementsByClassName('secondary_plates_check_all');
+	secondarychecked=scp[0].checked;
+	 if (secondarychecked == true)
+          {
+           secondarychecked = true
+          }
+        else
+          {
+          secondarychecked = false
+          }
+	for (var i =0; i < sp.length; i++) 
+	{
+	 sp[i].checked = secondarychecked;
+	}
+}
 
--- app/controllers/identities_controller.rb
+++ app/controllers/identities_controller.rb
@@ -17,33 +17,54 @@ class IdentitiesController < ApplicationController
     @secondaryplates = @user.secondaryplates 
     @friends = User.find(:all)
     
-   #For partial_____________________________
-   
+    #PARTIAL__________
+    if params[:plates]!=nil
+     @myp = params[:plates]
+     else
+     @myp = Array.new
+    end
+    if params[:secondaryplates]!=nil
+     @sp = params[:secondaryplates]
+    else
+     @sp = Array.new
+    end
+    if params[:friends]!=nil
+     @f = params[:friends]
+    else
+     @f = Array.new
+    end
 
-   if params[:commit] == 'update'
+    if params[:commit] == 'update'
      @startup = 0
      @allfeeds = getFeeds(@myp,@sp,@f)
-   else
+    else
      @startup = 1
      @allfeeds = getFeedsStartup(@plates.collect(&:id),@secondaryplates.collect(&:id))
-   end
-   
-   #@feeds = @allfeeds.sort_by { |obj| obj.published_at } sorted = @records.sort_by &:created_at
-   if @allfeeds
-    @feeds = @allfeeds.sort_by &:created_at
-    @feeds = @allfeeds.reverse
-  end
+    end
+
+
+    if @allfeeds
+     @feeds = @allfeeds.sort_by &:created_at
+     @feeds = @allfeeds.reverse
+    end
+
+    @t = Time.now
+    @h1 = @t - 1.hour
+
+    @allfeeds = @feeds
+
+    if @allfeeds
+    @feeds = @allfeeds.paginate(:page => params[:page], :per_page => 7)
+    end
+    #____________
     
-   @t = Time.now
-   @h1 = @t - 1.hour
-   
-   #@allfeeds = @feeds.sort { |p1, p2| p2.impressionist_count(:filter=>:all, :start_date=>@t) <=> p1.impressionist_count(:filter=>:all, :start_date=>@h1)}
-   @allfeeds = @feeds
     
-  # feed = @user.feeds(:order => "created_at DESC").sort { |p1, p2| p2.impressionist_count(:filter=>:all, :start_date=>@h1) <=> p1.impressionist_count(:filter=>:all, :start_date=>@h1)}
-   if @allfeeds
-     @feeds = @allfeeds.paginate(:page => params[:page], :per_page => 7)
-   end
+    
+    
+    
+    #@feeds = @allfeeds.sort_by { |obj| obj.published_at } sorted = @records.sort_by &:created_at
+    #@allfeeds = @feeds.sort { |p1, p2| p2.impressionist_count(:filter=>:all, :start_date=>@t) <=> p1.impressionist_count(:filter=>:all, :start_date=>@h1)}
+    # feed = @user.feeds(:order => "created_at DESC").sort { |p1, p2| p2.impressionist_count(:filter=>:all, :start_date=>@h1) <=> p1.impressionist_count(:filter=>:all, :start_date=>@h1)}
 
     respond_to do |format|
        format.html 
