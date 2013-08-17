8df5ac93ca8ce150a9f3085e7c2bb824a4a8e930
Otto Velander <o.velander@gmail.com>
2013-08-17 21:16:33 +0200

    fixed login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,15 +8,15 @@ class HomeController < ApplicationController
 	@link_feed_type = 'mostread'
   if params[:feed_type] == nil || params[:feed_type] == 'mostread'
     feed = Feed.find(:all, :order => "created_at DESC").sort { |p1, p2| p2.impressionist_count(:filter=>:all, :start_date=>@h1) <=> p1.impressionist_count(:filter=>:all, :start_date=>@h1)}
-    @feeds = feed.paginate(:page => params[:page], :per_page => 7)
+    @feeds = feed.paginate(:page => params[:page], :per_page => 14)
     @link_feed_type = 'mostread'
   elsif params[:feed_type] == 'mostrecent'
     feed = Feed.find(:all, :order => "created_at DESC")
-    @feeds = feed.paginate(:page => params[:page], :per_page => 7)
+    @feeds = feed.paginate(:page => params[:page], :per_page => 14)
     @link_feed_type = 'mostrecent'
   elsif params[:feed_type] == 'mostcommented'
     feed = Feed.find(:all, :order => "created_at DESC").sort_by { |feed| feed.comments.count }.reverse
-    @feeds = feed.paginate(:page => params[:page], :per_page => 7)
+    @feeds = feed.paginate(:page => params[:page], :per_page => 14)
     @link_feed_type = 'mostcommented'
   end
  
